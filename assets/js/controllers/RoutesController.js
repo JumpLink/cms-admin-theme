6d12833ede4503be2989f43fb1bb35a85f21b14f
JumpLink <pascal@jumplink.eu>
2015-08-05 20:21:21 +0200

    inital Routes

--- assets/js/controllers/RoutesController.js
+++ assets/js/controllers/RoutesController.js
@@ -1,21 +1,43 @@
-jumplink.cms.controller('RoutesController', function($rootScope, $scope, $log, ThemeService) {
-  $scope.themeSettings = {};
+jumplink.cms.controller('RoutesController', function($rootScope, $scope, $log, RoutesService) {
+  // $scope.themeSettings = {};
 
   $rootScope.$watch('selectedHost', function(newValue, oldValue) {
     $log.debug("[ThemesController] selectedHost changed from",oldValue,"to",newValue);
     if(angular.isDefined(newValue)) {
-      ThemeService.findByHost(newValue, function(err, themes) {
-        $scope.themeSettings = themes;
-      });
+
     }
   });
   
   $scope.save = function() {
     $log.debug('themeSettings', $scope.themeSettings);
-    ThemeService.updateOrCreateEachByHost($rootScope.selectedHost, $scope.themeSettings.available, function(data) {
+    RoutesService.updateOrCreateEachByHost($rootScope.selectedHost, $scope.themeSettings.available, function(data) {
       // $scope.themeSettings = data;
       $log.debug(data);
     });
   }
-  
+
+
+  $scope.add = function() {
+    if($rootScope.authenticated) {
+      SubnavigationService.add($scope.navs, {page:page}, function(err, navs) {
+        if(err) $log.error("Error: On add navs!", err);
+        $log.debug("add navs done!", navs);
+      });
+    }
+  }
+
+  $scope.moveForward = function(index, content) {
+    SortableService.moveForward(index, $scope.contents, function(err, contents) {
+      if(err) $log.error("Error: On move content forward!", err);
+      else $scope.contents = contents;
+    });
+  }
+
+  $scope.moveBackward = function(index, content) {
+    SortableService.moveBackward(index, $scope.contents, function(err, contents) {
+      if(err) $log.error("Error: On move content backward!", err);
+      else $scope.contents = contents;
+    });
+  } 
+
 });
