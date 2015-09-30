0a3661f7d631be71fd34eed54758c1fe04e34d30
JumpLink <pascal@jumplink.eu>
2015-09-30 19:38:49 +0200

    fixes for new cms angular module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,43 @@
 jumplink.cms = angular.module('jumplink.cms', [
-  'ui.router'                 // AngularUI Router: https://github.com/angular-ui/ui-router
-  , 'ngAnimate'               // ngAnimate: https://docs.angularjs.org/api/ngAnimate
-  , 'ngSanitize'              // ngSanitize: https://docs.angularjs.org/api/ngSanitize
-  , 'sails.io'                // angularSails: https://github.com/balderdashy/angularSails
-  //, 'webodf'                  // custom module
-  , 'FBAngular'               // angular-fullscreen: https://github.com/fabiobiondi/angular-fullscreen
-  , 'mgcrea.ngStrap'          // AngularJS 1.2+ native directives for Bootstrap 3: http://mgcrea.github.io/angular-strap/
-  , 'angularMoment'           // Angular.JS directive and filters for Moment.JS: https://github.com/urish/angular-moment
-  // , 'wu.masonry'              // A directive to use masonry with AngularJS: http://passy.github.io/angular-masonry/
-  , 'angular-carousel'        // An AngularJS carousel implementation optimised for mobile devices: https://github.com/revolunet/angular-carousel
-  // , 'textAngular'             // A radically powerful Text-Editor/Wysiwyg editor for Angular.js: https://github.com/fraywing/textAngular
-  , 'angular-medium-editor'   // AngularJS directive for Medium.com editor clone: https://github.com/thijsw/angular-medium-editor
-  , 'ui.ace'                  // This directive allows you to add ACE editor elements: https://github.com/angular-ui/ui-ace
- // , 'leaflet-directive'       // AngularJS directive to embed an interact with maps managed by Leaflet library: https://github.com/tombatossals/angular-leaflet-directive
-  , 'toaster'                 // AngularJS Toaster is a customized version of "toastr" non-blocking notification javascript library: https://github.com/jirikavi/AngularJS-Toaster
-  //, 'angularFileUpload'       // Angular File Upload is a module for the AngularJS framework: https://github.com/nervgh/angular-file-upload
-  , 'angular-filters'         // Useful filters for AngularJS: https://github.com/niemyjski/angular-filters
-  , 'ngDraggable'             // Drag and drop module for Angular JS: https://github.com/fatlinesofcode/ngDraggable
-  , 'toggle-switch'           // AngularJS Toggle Switch: https://github.com/JumpLink/angular-toggle-switch
-  , 'ngAsync'
-  , 'ngFocus'
-  , 'ngHistory'
-  , 'jumplink.cms.content'
-  , 'jumplink.cms.sortable'
-  , 'jumplink.cms.utilities'
-  , 'jumplink.cms.subnavigation'
-  , 'jumplink.cms.info'
-  , 'jumplink.cms.config'
-  //, 'jumplink.cms.event'
-  , 'jumplink.cms.user'
-  , 'jumplink.cms.theme'
-  //, 'jumplink.cms.gallery'
-  , 'jumplink.cms.admin'
-  , 'jumplink.cms.session'
-  , 'jumplink.cms.multisite'
-  , 'jumplink.cms.routes'
-  , 'jumplink.cms.sidebar'
-  , 'jumplink.cms.toolbar'
+  'ui.router',                 // AngularUI Router: https://github.com/angular-ui/ui-router
+  'ngAnimate',               // ngAnimate: https://docs.angularjs.org/api/ngAnimate
+  'ngSanitize',              // ngSanitize: https://docs.angularjs.org/api/ngSanitize
+  'sails.io',                // angularSails: https://github.com/balderdashy/angularSails
+  //, 'webodf',                  // custom module
+  'FBAngular',               // angular-fullscreen: https://github.com/fabiobiondi/angular-fullscreen
+  'mgcrea.ngStrap',          // AngularJS 1.2+ native directives for Bootstrap 3: http://mgcrea.github.io/angular-strap/
+  'angularMoment',           // Angular.JS directive and filters for Moment.JS: https://github.com/urish/angular-moment
+  // , 'wu.masonry',              // A directive to use masonry with AngularJS: http://passy.github.io/angular-masonry/
+  'angular-carousel',        // An AngularJS carousel implementation optimised for mobile devices: https://github.com/revolunet/angular-carousel
+  // , 'textAngular',             // A radically powerful Text-Editor/Wysiwyg editor for Angular.js: https://github.com/fraywing/textAngular
+  'angular-medium-editor',   // AngularJS directive for Medium.com editor clone: https://github.com/thijsw/angular-medium-editor
+  'ui.ace',                  // This directive allows you to add ACE editor elements: https://github.com/angular-ui/ui-ace
+ // , 'leaflet-directive',       // AngularJS directive to embed an interact with maps managed by Leaflet library: https://github.com/tombatossals/angular-leaflet-directive
+  'toaster',                 // AngularJS Toaster is a customized version of "toastr" non-blocking notification javascript library: https://github.com/jirikavi/AngularJS-Toaster
+  //, 'angularFileUpload',       // Angular File Upload is a module for the AngularJS framework: https://github.com/nervgh/angular-file-upload
+  'angular-filters',         // Useful filters for AngularJS: https://github.com/niemyjski/angular-filters
+  'ngDraggable',             // Drag and drop module for Angular JS: https://github.com/fatlinesofcode/ngDraggable
+  'toggle-switch',           // AngularJS Toggle Switch: https://github.com/JumpLink/angular-toggle-switch
+  'ngAsync',
+  'ngFocus',
+  'jumplink.cms.history',
+  'jumplink.cms.content',
+  'jumplink.cms.sortable',
+  'jumplink.cms.utilities',
+  'jumplink.cms.subnavigation',
+  'jumplink.cms.info',
+  'jumplink.cms.config',
+  //, 'jumplink.cms.event',
+  'jumplink.cms.user',
+  'jumplink.cms.theme',
+  //, 'jumplink.cms.gallery',
+  'jumplink.cms.admin',
+  'jumplink.cms.session',
+  'jumplink.cms.multisite',
+  'jumplink.cms.routes',
+  'jumplink.cms.sidebar',
+  'jumplink.cms.toolbar',
+  'jumplink.cms.signin',
 ]);
 
 jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvider, $provide, $logProvider) {
@@ -56,7 +57,7 @@ jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvi
     controller: 'LayoutController',
     resolve: {
       authenticated: function (SessionService) {
-        return SessionService.authenticated();
+        return SessionService.needToBeAuthenticated();
       },
       sites: function(MultisiteService) {
         return MultisiteService.resolveNames({});
@@ -131,7 +132,7 @@ jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvi
     url: '/users'
     , resolve: {
       authenticated: function (SessionService) {
-        return SessionService.authenticated();
+        return SessionService.needToBeAuthenticated();
       },
       users: function($sailsSocket, $log) {
         return $sailsSocket.get('/user').then (function (data) {
@@ -162,7 +163,7 @@ jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvi
     url: '/user/:index'
     , resolve: {
       authenticated: function (SessionService) {
-        return SessionService.authenticated();
+        return SessionService.needToBeAuthenticated();
       },
       user: function($sailsSocket, $stateParams, $log) {
         return $sailsSocket.get('/user'+'/'+$stateParams.index).then (function (data) {
@@ -194,7 +195,7 @@ jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvi
     url: '/new/user'
     , resolve: {
       authenticated: function (SessionService) {
-        return SessionService.authenticated();
+        return SessionService.needToBeAuthenticated();
       },
       user: function() {
         return {
@@ -224,7 +225,7 @@ jumplink.cms.config( function($stateProvider, $urlRouterProvider, $locationProvi
     url: '/cms'
     , resolve: {
       authenticated: function (SessionService) {
-        return SessionService.authenticated();
+        return SessionService.needToBeAuthenticated();
       },
       info: function(CmsService, $log) {
         $log.debug("start get cms info");
--- assets/js/controllers/ErrorController.js
+++ assets/js/controllers/ErrorController.js
@@ -1,10 +1,10 @@
-jumplink.cms.controller('ErrorController', function($scope, $log, $stateParams, SessionService, $state) {
+jumplink.cms.controller('ErrorController', function($rootScope, $scope, $log, $stateParams, SessionService, SigninService, $state) {
   $scope.error = $stateParams.error;
 
   $scope.signin = function () {
     $log.debug($scope.user);
-    $scope.user.role = 'superadmin';
-    SessionService.create($scope.user, function (error, result) {
+    // $scope.user.role = 'superadmin';
+    SigninService.signin($scope.user, false, function (error, result) {
       if(error) $scope.error = error;
       else {
         $state.go('layout.home');
@@ -12,5 +12,28 @@ jumplink.cms.controller('ErrorController', function($scope, $log, $stateParams, 
       $log.debug(result);
     });
   }
+
+  $scope.signin = function () {
+    $log.debug("[SigninController.signin]", $scope.user);
+    SigninService.signin($scope.user, false, function (error, result) {
+      if(error) {
+        $scope.error = error;
+        return $scope.error ;
+      }
+      if(result.authenticated) {
+        $rootScope.authenticated = result.authenticated;
+        $rootScope.user = result.user;
+        $rootScope.site = result.site;
+        SessionService.isAuthenticated(function (err, isAuthenticated) {
+          $rootScope.authenticated = isAuthenticated;
+          SessionService.superadmin(function (err, isSuperadmin) {
+            $rootScope.superadmin = isSuperadmin;
+            $state.go('layout.home');
+          });
+        });        
+      }
+      $log.debug("[SigninController.signin]", result);
+    });
+  };
   
 });
